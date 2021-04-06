4557401203713aa785c4d1bbd060b5c95f60fd90
Randy Barents <33863457+randybarents@users.noreply.github.com>
2021-04-06 09:14:50 +0200

    Data saving and loading

--- MW_RollerRates/DataLayer/Access/DBAccess.cs
+++ MW_RollerRates/DataLayer/Access/DBAccess.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
 
 namespace DataLayer
 {
@@ -6,7 +12,41 @@ namespace DataLayer
     {
         public string GetConnectionString()
         {
-            return "";
+            return "server=;database=;username=";
+        }
+
+        public List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                return con.Query<T>(sql).ToList();
+            }
+        }
+
+        public T LoadFirstData<T>(string sql)
+        {
+            using (IDbConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                {
+                    return con.Query<T>(sql).FirstOrDefault();
+                }
+            }
+        }
+
+        public void SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                con.Execute(sql, data);
+            }
+        }
+
+        public void DeleteData<T>(string sql, T data)
+        {
+            using (IDbConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                con.Execute(sql, data);
+            }
         }
     }
 }
