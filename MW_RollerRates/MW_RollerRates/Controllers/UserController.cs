fefa04330dce05613e36f3ba4e5d027d0a0770a2
Randy Barents <33863457+randybarents@users.noreply.github.com>
2021-04-06 09:51:16 +0200

    UserController create user

---
+++ MW_RollerRates/MW_RollerRates/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLayer.DTModels;
+using DataLayer.DTProcessors;
+using MW_RollerRates.Models;
+
+namespace MW_RollerRates.Controllers
+{
+    public class UserController : Controller
+    {
+        UserProcessor Processor = new UserProcessor();
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUser(UserCreateModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                Processor.CreateUser(
+                    user.Email,
+                    user.Password,
+                    user.DisplayName,
+                    user.Description
+                    );
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ MW_RollerRates/MW_RollerRates/Models/UserCreateModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MW_RollerRates.Models
+{
+    public class UserCreateModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "E-Mail")]
+        [Required(ErrorMessage = "Enter an e-mail address")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Enter a valid e-mail address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Confirm e-mail")]
+        [Display(Name = "Confirm e-mail")]
+        [Compare("Email", ErrorMessage = "E-mail must match the above.")]
+        public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage = "Enter a password")]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Password has to be a minimum of 5 characters and a maximum of 15")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password")]
+        [Display(Name = "Confirm password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password must match the above.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Enter a name for your profile!")]
+        [Display(Name = "DisplayName")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Name must be at least 2 characters")]
+        public string DisplayName { get; set; }
+
+        [Required(ErrorMessage = "Enter a description for your profile!")]
+        [Display(Name = "Description")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Description must be at least 2 characters")]
+        public string Description { get; set; }
+    }
+}
