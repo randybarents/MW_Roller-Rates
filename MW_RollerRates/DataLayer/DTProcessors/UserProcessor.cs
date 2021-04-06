28f5af25c3a9e9cb52fec9b37e52d14e06839d12
Randy Barents <33863457+randybarents@users.noreply.github.com>
2021-04-06 09:38:02 +0200

    Create user method
    Added create user to userprocessor

---
+++ MW_RollerRates/DataLayer/DTProcessors/UserProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.DTProcessors
+{
+    public class UserProcessor
+    {
+        DBAccess Access = new DBAccess();
+
+        public void CreateUser(string email, string password, string displayname, string description)
+        {
+            DTModels.UserModel data = new DTModels.UserModel
+            {
+                Email = email,
+                Password = password,
+                DisplayName = displayname,
+                Description = description
+            };
+            string sql = @"INSERT INTO user (Email, Password, DisplayName, Description)
+                            VALUES(@Email, @Password, @DisplayName, @Description);";
+            Access.SaveData(sql, data);
+        }
+        
+    }
+}
