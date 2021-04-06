d787e303d0bc4148622309cbd52ce6389e2057f7
Randy Barents <33863457+randybarents@users.noreply.github.com>
2021-04-06 09:55:19 +0200

    Create user view

--- MW_RollerRates/DataLayer/DTModels/UserModel.cs
+++ MW_RollerRates/DataLayer/DTModels/UserModel.cs
@@ -10,5 +10,6 @@ namespace DataLayer.DTModels
         public string Email { get; set; }
         public string Password { get; set; }
         public string DisplayName { get; set; }
+        public string Description { get; set; }
     }
 }
